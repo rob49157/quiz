7dd5dc3aae5742f11667bbf58adbc22f9a88265c
roberto <robertopaipo@hotmail.com>
2021-09-19 16:55:49 -0400

    functions

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var question4 = document.querySelectorAll(".question4")
 var question5 = document.querySelectorAll(".question5") 
 var start= document.getElementById("start")
 var start_btn= document.getElementById("start_btn")
+var question_btn= document.getElementById ("q_btn1")
 
 var score =  " "
 var time = 35;
@@ -12,11 +13,9 @@ var questionscore = 20
 
 // start timer and create timer
 start_btn.addEventListener("click",timer)
-console.log('hgd')
 
  function timer(){
      var timeremaining = " "
-     console.log('hjjhg')
      var timeinterval= setInterval(function(){ 
         time--;
         start.textContent= time + " time remaining"
@@ -25,15 +24,23 @@ console.log('hgd')
         time += timeremaining}, 1000); }
      
      
-    
-   
-
- 
-
-
-
 // need to create functions(event) for the question with else stastements
+question_btn.addEventListener("click", firstquestion)
+function firstquestion(){
+    var correct= question1[5]
+    console.log(question1)
+    if ( correct === true){
+        questionscore +20;
+        question1.textContent= "correct"}
+    else( correct != false)
+    {
+        questionscore -20;
+        question1.textContent= "wrong"
+    }
+    questionscore+= score
+    
+    
+}
 
-// need to create timer
 
 // need to create final score and save to "localStorage"
