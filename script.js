59cb473d512dcdf0aa0cad22922c0f5bfb22d0ce
roberto <robertopaipo@hotmail.com>
2021-09-20 13:43:05 -0400

    btn soolve

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var question4 = document.querySelector(".question4")
 var question5 = document.querySelector(".question5") 
 var start= document.getElementById("start")
 var start_btn= document.getElementById("start_btn")
-var question_btn= document.getElementById ("q_btn4","q_btn2","q_btn3","q_btn1")
+var question_btn= document.getElementById ("q_btn4")
 
 var score =  " "
 var time = 35;
@@ -28,22 +28,21 @@ start_btn.addEventListener("click",timer)
 question_btn.addEventListener("click", firstquestion)
 
 function firstquestion(){   
-    var correct = question1
-    console.log(question1)
-  // en el quiz en la primera pregunta si haces click en el ultimo choice, funciona pero se pone wrong en ves de correct como deveria ser. nose si el IF statement esta malo  
-    if ( correct === true){
-        questionscore +20;
-        question1.textContent= "correct"}
-    else( correct !== false)
+    
+    var correct = question_btn.value
+    // en el quiz en la primera pregunta si haces click en el ultimo choice, funciona pero se pone wrong en ves de correct como deveria ser. nose si el IF statement esta malo  
+  // los variables parace que estan bien. el "question_btn" es para la primera pregunta
+    if ( correct === "all of above"){
+        questionscore =20;
+        question1.textContent= "correct"} 
+    else
     {
-        questionscore -20;
+        questionscore =-20;
         question1.textContent= "wrong"
     }
-    return correct
-    questionscore+= score
+    score += questionscore
+
     
     
 }
-
-
 // need to create final score and save to "localStorage"
